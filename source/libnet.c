9428a65de8e75ab996925394ee10d61eaab6278d
sreeja <sreeja_nampoothiri@comcast.com>
2026-03-10 11:51:55 +0530

    coverity test

--- source/libnet.c
+++ source/libnet.c
@@ -107,7 +107,7 @@ libnet_status vlan_create(const char *if_name, int vid)
         libnet_status err = CNL_STATUS_FAILURE;
         char vlan_if_name[32] = {0};
         errno_t rc;
-        print("Test coverity workflow ,%s")
+        printf("Test coverity workflow ,%s");
 
         rc = sprintf_s(vlan_if_name, sizeof(vlan_if_name), "%s.%d", if_name,vid);
         ERR_CHK(rc);
